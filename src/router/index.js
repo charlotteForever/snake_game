f6d0f4db725aa0e7510e8e36ef3448671b43157e
charlotteForever <fernweh1001@163.com>
2022-03-06 15:12:14 +0800

    搭建环境&画出游戏面板

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+// @ts-ignore
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -16,7 +17,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // @ts-ignore
+    component: () => import('../views/About')
   }
 ]
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="game">
+    这是我的一个贪吃蛇小demo<br />
+    <canvas ref="panel" width="300" height="300"></canvas>
   </div>
 </template>
+
+<script>
+export default {
+  name: "snake_name",
+  data() {
+    return {
+      context: "",
+    };
+  },
+  mounted() {
+    // 先创建面板
+    /** @type {HTMLCanvasElement} */
+    this.context = this.$refs.panel.getContext("2d");
+    this.context.fillStyle = "white";
+    this.context.strokeStyle = "black";
+    // 控制画布的横向偏移、纵向偏移、画布长度、宽度
+    this.context.fillRect(
+      0,
+      0,
+      this.$refs.panel.width,
+      this.$refs.panel.height
+    );
+    // 绘制画布的边框
+    this.context.strokeRect(
+      0,
+      0,
+      this.$refs.panel.width,
+      this.$refs.panel.height
+    );
+  },
+};
+</script>
+
+<style lang="css" scoped>
+/* .game {
+  width: 300;
+  height: 300;
+} */
+</style>
